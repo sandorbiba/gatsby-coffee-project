400122adf1a4cbe4691bb7a1ac13ce9d342e1b06
Sandor Biba <sandorbiba@gmail.com>
2020-01-24 17:07:39 +0100

    add Navbar

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,10 +9,17 @@ import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
+import Navbar from "./Globals/Navbar"
+
 import "./bootstrap.min.css"
 import "./layout.css"
 
-const Layout = ({ children }) => <>{children}</>
+const Layout = ({ children }) => (
+  <>
+    <Navbar />
+    {children}
+  </>
+)
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
